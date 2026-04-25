26c24a056478b1006b2aeba421fd53b0db280620
lakshy1 <lakshyasehgal01@gmail.com>
2026-04-26 04:50:42 +0530

    changes in plugions

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
   plugins: [
     react(),
-    viteImagemin({
-      gifsicle: { optimizationLevel: 7 },
-      optipng: { optimizationLevel: 7 },
-      mozjpeg: { quality: 75 },
-      pngquant: { quality: [0.7, 0.9], speed: 4 },
-      webp: { quality: 75 },
-    }),
   ],
 })
